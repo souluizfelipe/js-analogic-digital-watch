0b9e77ffc518ef9fd2284644af4f186616ff7496
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-07-21 11:26:06 -0300

    add style to watch pointers to rotate on updateClock function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const digitalElement = document.querySelector('.digital');
-
+const pointerSec = document.querySelector('.p_s');
+const pointerMin = document.querySelector('.p_m');
+const pointerHour = document.querySelector('.p_h');
 
 function updatedClock() {
   const time = new Date();
@@ -8,6 +10,14 @@ function updatedClock() {
   const second = time.getSeconds();
 
   digitalElement.textContent = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
+
+  let secDeg = ((360 / 60) * second) - 90;
+  let minDeg = ((360 / 60) * minute) - 90;
+  let hourDeg = ((360 / 12) * hour) - 90;
+
+  pointerSec.style.transform = `rotate(${secDeg}deg)`;
+  pointerMin.style.transform = `rotate(${minDeg}deg)`;
+  pointerHour.style.transform = `rotate(${hourDeg}deg)`;
 }
 ;
 function fixZero(time) {
@@ -15,3 +25,4 @@ function fixZero(time) {
 };
 
 setInterval(updatedClock, 1000);
+updatedClock();
